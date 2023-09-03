6223187b5f96371846949b3e3b6cb5d8d41d697f
Joe Wilson <wilsonmangunsong@gmail.com>
2023-09-03 16:40:52 +0700

    using-error-handler

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v3"
 )
@@ -8,6 +10,23 @@ import (
 // Middleware JWT Function
 func NewAuthMiddleware(secret string) fiber.Handler {
 	return jwtware.New(jwtware.Config{
-		SigningKey: []byte(secret),
+		SigningKey:   []byte(secret),
+		ErrorHandler: jwtError,
 	})
+}
+
+type BaseResponseFailed struct {
+	Message string `json:"message"`
+}
+
+// using func error handler
+func jwtError(c *fiber.Ctx, err error) error {
+	fmt.Println("jwt error", err)
+	var resp BaseResponseFailed
+	if err.Error() == "Missing or malformed JWT" {
+		resp.Message = err.Error()
+		return c.Status(fiber.StatusBadRequest).JSON(resp)
+	}
+	resp.Message = err.Error()
+	return c.Status(fiber.StatusUnauthorized).JSON(resp)
 }
